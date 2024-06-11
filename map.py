07c9e16d80ee201665f84d11a20444899e900fd6
Safal Shrestha <computerstha@gmail.com>
2024-06-11 20:04:28 +0545

    Added dissolve in map and analysis

--- map.py
+++ map.py
@@ -1,14 +1,25 @@
 import pandas  as pd 
+from pandas import _typing
 import matplotlib.pyplot as plt
 import geopandas as gpd 
 import os 
 import regex as re 
 
+
+
+
 class Map:
+    '''
+    Nepal Map\n
+    This is a new nepal map. It has 77 districts.
+    
+    '''
     __folder_path = './districts_geojson'
     map_gdfs = []
+    __original_gdfs = []
     def __init__(self):
-        self.map_gdfs = self.__concatGeoDataFrame()
+        self.__original_gdfs = self.__concatGeoDataFrame()
+        self.map_gdfs = self.__original_gdfs.copy()
         self.__fixDifference() 
     
     def __loadgeoJson(self):
@@ -31,6 +42,14 @@ class Map:
         #check for the difference
         self.map_gdfs = pd.merge(self.map_gdfs, dataFrame,on='DISTRICT',how='outer')
         self.map_gdfs=self.map_gdfs.drop('District',axis=1)
+        return self.map_gdfs
+    def merge(self,dataFrame,on,how:_typing.MergeHow='inner'):
+        '''Merge the dataframe map_gdfs'''
+        if not isinstance(dataFrame, pd.DataFrame):
+            raise "Pandas DataFrame is required"
+        #check for the difference
+        self.map_gdfs = pd.merge(self.map_gdfs, dataFrame,on=on,how=how)
+        self.map_gdfs.reset_index(inplace=True)
         return self.map_gdfs
 
     def __fixDifference(self):
@@ -55,3 +74,54 @@ class Map:
             self.map_gdfs['DISTRICT'] = self.map_gdfs['DISTRICT'].str.replace(pattern, dist, regex=True)
 
         return None
+    
+
+    def __repairInvalidArgs(self):
+        # Clean geometries by buffering with a distance of 0
+        self.map_gdfs['geometry'] = self.map_gdfs['geometry'].buffer(0)
+
+        # Further clean invalid geometries
+        def repair_geometry(geom):
+            if not geom.is_valid:
+                return geom.buffer(0)
+            return geom
+
+        self.map_gdfs['geometry'] = self.map_gdfs['geometry'].apply(repair_geometry)
+        
+        # Remove any remaining invalid geometries
+        self.map_gdfs = self.map_gdfs[self.map_gdfs.is_valid]
+
+
+    def dissolve_by_district(self):
+        '''Dissolve polygons by district name'''
+        #repair the invalid args 
+        self.__repairInvalidArgs()
+        # Dissolve polygons by district name
+        self.map_gdfs = self.map_gdfs.dissolve(by='DISTRICT')
+        return self.map_gdfs
+    
+    def dissolve_by_zone(self):
+        '''Dissolve polygons by Zone name'''
+        # Clean geometries by buffering with a distance of 0
+          #repair the invalid args 
+        self.__repairInvalidArgs()
+        # Dissolve polygons by Zone name
+        self.map_gdfs = self.map_gdfs.dissolve(by='Zone')
+        return self.map_gdfs
+    def dissolve_by_province(self):
+        '''Dissolve polygons by PROVINCE name'''
+        # Clean geometries by buffering with a distance of 0
+          #repair the invalid args 
+        self.__repairInvalidArgs()
+        # Dissolve polygons by Zone name
+        self.map_gdfs = self.map_gdfs.dissolve(by='PROVINCE')
+        return self.map_gdfs
+
+    def reset(self):
+        '''
+        This function is used to reset\n
+        It resets all the dissolve and merge operations 
+        '''
+        self.map_gdfs = self.__original_gdfs.copy()
+        self.__fixDifference() 
+        return self.map_gdfs
